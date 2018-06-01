5dec92b7d42ada719bce745bab4bac1c1e4fc6eb
MichaelErichsen <michael.erichsen@xact.dk>
2018-06-01 10:33:34 +0200

    Clean Up

--- HRE--History-Research-Environment/org.historyresearchenvironment/bundles/org.historyresearchenvironment.client/src/org/historyresearchenvironment/client/serverinterface/package-info.java
+++ HRE--History-Research-Environment/org.historyresearchenvironment/bundles/org.historyresearchenvironment.client/src/org/historyresearchenvironment/client/serverinterface/package-info.java
@@ -1,6 +1,6 @@
 /**
- * @version 28. maj 2018
+ * @version 30. maj 2018
  * @author Michael Erichsen, &copy; History Research Environment Ltd., 2018
  *
  */
-package org.historyresearchenvironment.dialogs;
+package org.historyresearchenvironment.client.serverinterface;
--- HRE--History-Research-Environment/org.historyresearchenvironment/bundles/org.historyresearchenvironment.client/src/serverinterface/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @version 30. maj 2018
- * @author Michael Erichsen, &copy; History Research Environment Ltd., 2018
- *
- */
-package serverinterface;
