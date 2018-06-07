4c8c8cad1b6ff10a9cd26a9bc1fb4e64bb802049
MichaelErichsen <michael.erichsen@xact.dk>
2018-06-07 20:53:40 +0200

    Initial preferences pages

---
+++ HRE--History-Research-Environment/org.historyresearchenvironment/bundles/org.historyresearchenvironment.client/src/org/historyresearchenvironment/client/preferences/HrePreferencePage.java
@@ -0,0 +1,63 @@
+package org.historyresearchenvironment.client.preferences;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.resource.ImageDescriptor;
+
+/**
+ * @version 2018-06-07
+ * @author Michael Erichsen, &copy; History Research Environment Ltd., 2018
+ *
+ */
+public class HrePreferencePage extends FieldEditorPreferencePage {
+
+	/**
+	 * Constructor
+	 *
+	 */
+	public HrePreferencePage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @param style
+	 */
+	public HrePreferencePage(int style) {
+		super(style);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @param title
+	 * @param style
+	 */
+	public HrePreferencePage(String title, int style) {
+		super(title, style);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @param title
+	 * @param image
+	 * @param style
+	 */
+	public HrePreferencePage(String title, ImageDescriptor image, int style) {
+		super(title, image, style);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.preference.FieldEditorPreferencePage#createFieldEditors()
+	 */
+	@Override
+	protected void createFieldEditors() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
