88db43e1610e6ff9036427bec69dfe8386fdb7ff
MichaelErichsen <michael.erichsen@xact.dk>
2018-06-09 14:19:42 +0200

    21: Update About dialog
    Task-Url: https://github.com/History-Research-Environment/HRE--History-Research-Environment/issues/issues/21

--- HRE--History-Research-Environment/org.historyresearchenvironment/bundles/org.historyresearchenvironment.client/src/org/historyresearchenvironment/client/handlers/AboutHandler.java
+++ HRE--History-Research-Environment/org.historyresearchenvironment/bundles/org.historyresearchenvironment.client/src/org/historyresearchenvironment/client/handlers/AboutHandler.java
@@ -1,12 +1,21 @@
 package org.historyresearchenvironment.client.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.historyresearchenvironment.client.dialogs.AboutDialog;
 
+/**
+ * @version 2018-06-09
+ * @author Michael Erichsen, &copy; History Research Environment Ltd., 2018
+ *
+ */
 public class AboutHandler {
+	/**
+	 * @param shell
+	 */
 	@Execute
 	public void execute(Shell shell) {
-		MessageDialog.openInformation(shell, "About", "History Research Environment V0.1");
+		final AboutDialog dialog = new AboutDialog(shell);
+		dialog.open();
 	}
 }
